eadb650bd360c7b259c5c2068633edf3bcd21451
Rani Pandit <ranipandit2610@gmail.com>
2024-03-27 22:45:58 +0530

    venu first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <p className='text-3xl text-sky-700 font-medium'> Hello World</p>
+      <p className='text-3xl text-sky-700 font-medium'>venu</p>
       <Button variant='destructive'>Click Me</Button>
     </>
   )
